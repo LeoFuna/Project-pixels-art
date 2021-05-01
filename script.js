07a650d84879aeb3bcc4adb7a7ecd7e4c4310bcb
LeoFuna <leo.funa@gmail.com>
2021-05-01 07:16:11 -0300

    [REFA] Logica define board implementada, min 5 max 12

--- script.js
+++ script.js
@@ -1,49 +1,54 @@
 // 1366 x 768
-//Parte de geração do board, essa parte devo refatorar melhor!!!
-// function create5PixelDefault(newLinesIndex) {
-//   for (let index = 0; index < 5; index += 1) {
-//     const newPixel = document.createElement('div');
-//     newPixel.className = 'pixel';
-//     newPixel.style.marginRight = '4px';
-//     document.querySelectorAll('.pixel-line')[newLinesIndex].appendChild(newPixel);
-//   }
-// }
-// function createLines(quantity) {
-//   let newLinesIndex = 5;
-//   const realQuantity = quantity - newLinesIndex;
-//   for (let index = 0; index < realQuantity; index += 1) {
-//     const newLine = document.createElement('div');
-//     newLine.className = 'pixel-line';
-//     document.querySelector('#pixel-board').appendChild(newLine);
-//     create5PixelDefault(newLinesIndex);
-//     newLinesIndex += 1;
-//   }
-// }
-// function createPixels(quantity) {
-//   const theLines = document.querySelectorAll('.pixel-line');
-//   for (let index = 0; index < quantity; index += 1) {
-//     for (let indexInner = 5; indexInner < quantity; indexInner += 1) {
-//       const newPixel = document.createElement('div');
-//       newPixel.className = 'pixel';
-//       newPixel.style.marginRight = '4px';
-//       theLines[index].appendChild(newPixel);
-//     }
-//   }
-// }
+// Parte de geração do board, essa parte devo refatorar melhor!!!
+function create5PixelDefault(newLinesIndex) {
+  for (let index = 0; index < 5; index += 1) {
+    const newPixel = document.createElement('div');
+    newPixel.className = 'pixel';
+    newPixel.style.marginRight = '4px';
+    document.querySelectorAll('.pixel-line')[newLinesIndex].appendChild(newPixel);
+  }
+}
+function createLines(quantity) {
+  let newLinesIndex = 5;
+  const realQuantity = quantity - newLinesIndex;
+  for (let index = 0; index < realQuantity; index += 1) {
+    const newLine = document.createElement('div');
+    newLine.className = 'pixel-line';
+    document.querySelector('#pixel-board').appendChild(newLine);
+    create5PixelDefault(newLinesIndex);
+    newLinesIndex += 1;
+  }
+}
+function createPixels(quantity) {
+  const theLines = document.querySelectorAll('.pixel-line');
+  for (let index = 0; index < quantity; index += 1) {
+    for (let indexInner = 5; indexInner < quantity; indexInner += 1) {
+      const newPixel = document.createElement('div');
+      newPixel.className = 'pixel';
+      newPixel.style.marginRight = '4px';
+      theLines[index].appendChild(newPixel);
+    }
+  }
+}
 
-// function defineBoard() {
-//   const boardSize = document.querySelector('#board-size').value;
-//   const lineDivs = document.querySelectorAll('.pixel-line')
-//   for (let index = 5; index < lineDivs.length; index += 1) {
-//     document.querySelector('#pixel-board').removeChild(lineDivs[5]);
-//   }
-//   createLines(boardSize);
-//   createPixels(boardSize);
-// }
+function defineBoard() {
+  let boardSize = document.querySelector('#board-size');
+  if (boardSize.value > 12 || boardSize.value < 5) {
+    boardSize.value = '';
+    alert("Só é possível tamanho entre 5 e 12");
+  } else {
+    const lineDivs = document.querySelectorAll('.pixel-line')
+    for (let index = 5; index < lineDivs.length; index += 1) {
+      document.querySelector('#pixel-board').removeChild(lineDivs[5]);
+    }
+    createLines(boardSize.value);
+    createPixels(boardSize.value);
+  }
+}
 
 
-// let buttonBoardSize = document.querySelector('#generate-board');
-// buttonBoardSize.addEventListener('click', defineBoard);
+let buttonBoardSize = document.querySelector('#generate-board');
+buttonBoardSize.addEventListener('click', defineBoard);
 
 //Código para selecionar a cor que vai ser usada como tinta
 const pixelColor = document.querySelectorAll('.color');
